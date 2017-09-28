34d21180bfbddfe17ae6b3ef68f075a0eea72d87
queenskid <andres.ibarra0716@gmail.com>
2017-09-28 13:17:44 -0700

    Starting on day4

--- app.js
+++ app.js
@@ -4,36 +4,46 @@ var hours = ['6 am', '7 am', '8 am', '9 am', '10 am', '11 am','12 pm', '1 pm', '
 var locations = [];
 var storeTable = document.getElementById('stores')
 
-function Store(storeLocation, minimumCustomers, maximumCustomers, cookiesPer) {
-  this.storeLocation = storeLocation;
+function Store(storeLocations, minimumCustomers, maximumCustomers, cookiesPer) {
+  this.storeLocations = storeLocations;
   this.minimumCustomers = minimumCustomers;
   this.maximumCustomers = maximumCustomers;
   this.cookiesPer = cookiesPer;
+  this.customersEachHour = [];
   this.cookiesPerHourArray = [];
   this.cookiesTotal = [];
+  // this.locations = [];
   locations.push(this);
+  customersPerHour.push(this);
 
+};
 
- };
- Store.prototype.customersPerHour = function(){
-   return Math.floor(Math.random() * (this.maximumCustomers - this.minimumCustomers + 1) + this.minimumCustomers);
- };
+new Store('First And Pike', 23, 65, 6.3);
+new Store('Seatac Airport', 3, 24, 1.2);
+new Store('Seattle Center', 11, 38, 3.7);
+new Store('Capitol Hill', 20, 38, 2.3);
+new Store('Alki', 2, 16, 4.6);
 
- Store.prototype.cookiesPerHour = function(){
+Store.prototype.customersPerHour = function(){
+  //  return Math.floor(Math.random() * (this.maximumCustomers - this.minimumCustomers + 1) + this.minimumCustomers);
+    for (var i = 0; i < hours.length; i++) {
+  this.customersEachHour.push(Math.random(this.minimumcustomers, this.maxcustomers + 1) + this.minimumcustomers);
+}
+};
+
+Store.prototype.cookiesPerHour = function(){
    return Math.floor(Math.random() * (this.customersPerHour(this.minimumCustomers,this.maximumCustomers)) * this.cookiesPer);
- };
+};
 
- Store.prototype.hourlyTotal = function() {
+Store.prototype.hourlyTotal = function() {
    for (var i = 0; i < hours.length; i++) {
-
-
-   }
- };
+  }
+};
 
 Store.prototype.render = function() {
   var trEl = document.createElement('tr');
   var tdEl = document.createElement('td');
-  tdEl.textContent = this.storeLocation;
+  tdEl.textContent = this.storeLocations;
   trEl.appendChild(tdEl);
 
   for (var i = 0; i < hours.length; i++){
@@ -65,18 +75,11 @@ function makeFooterRow(){
 
  for (var i = 0; i < hours.length; i++){
    thEl = document.createElement('th');
-   thEl.textContent = cookiesPerHour[i];
+   thEl.textContent = cookiesPerHourArray[i];
    trEl.appendChild(thEl);
    storeTable.appendChild(trEl);
  }
 };
-
-
-new Store('First And Pike', 23, 65, 6.3);
-new Store('Seatac Airport', 3, 24, 1.2);
-new Store('Seattle Center', 11, 38, 3.7);
-new Store('Capitol Hill', 20, 38, 2.3);
-new Store('Alki', 2, 16, 4.6);
 
 function storeRows(){
   for (var i in locations){
